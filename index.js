d3c3bab53311a52581cd938247a26986698c5a95
surya-032 <114859751+surya-032@users.noreply.github.com>
2024-01-01 07:59:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello" }');
+    res.send('{ "response": "Hello My Name is Surya and Bayu" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Kami membuat implementasi cicd" }');
+    res.send('{ "response": "Kami membuat project sederhana implementasi cicd" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": "Berhasill!!" }');
